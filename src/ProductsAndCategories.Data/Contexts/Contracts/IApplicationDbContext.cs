e6022fdecdad4a9de513a65ceaa8bdc0034fcd2a
Kolya Voitekhovich <kolya.vaitihovich09@gmail.com>
2023-09-28 19:53:14 +0300

    Add db context

---
+++ src/ProductsAndCategories.Data/Contexts/Contracts/IApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductsAndCategories.Data.Entities;
+
+namespace ProductsAndCategories.Data.Contexts.Contracts
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<ProductEntity> Products { get; set; }
+        DbSet<CategoryEntity> Categories { get; set; }
+
+        DbSet<TEntity> Set<TEntity>() where TEntity : class;
+
+        Task<int> SaveChangesAsync();
+    }
+}
