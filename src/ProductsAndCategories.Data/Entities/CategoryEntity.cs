244d01646313cf5cfb2b98ae004786cc13d576c0
Kolya Voitekhovich <kolya.vaitihovich09@gmail.com>
2023-09-28 14:45:43 +0300

    Add entities

---
+++ src/ProductsAndCategories.Data/Entities/CategoryEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace ProductsAndCategories.Data.Entities
+{
+    public class CategoryEntity : BaseEntity
+    {
+        public string Name { get; set; } = null!;
+
+        public IEnumerable<ProductEntity> Products { get; set; }
+    }
+}
